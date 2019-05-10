70c6c84ad0b77921eb09b354585c645801842db8
Marvin Tappauf <marvin.tappauf@gmail.com>
2019-05-10 22:52:06 +0200

    added table name

--- app/Config.php
+++ app/Config.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
+    protected $table = 'config';
     protected $fillable = ['socket1', 'socket2', 'socket3', 'socket4', 'socket5', 'socket6'];
 
 }
